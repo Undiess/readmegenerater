98bca572f2cf96b5a0835a9811d5b97e58f5342b
Undiess <nguyen.andy67@gmail.com>
2020-05-02 22:31:25 +0930

    promt question

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
+
+function promptQuestions(){
+
+
+const questions = [{
+    type: 'input',
+    name: 'projectTitle',
+    message:' What is the project Title: '
+    }]
+    
+
+return inquirer.prompt(questions)
+}
+
+promptQuestions()
+
+
