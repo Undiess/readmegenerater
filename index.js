f1f41e94c5563200b05a8419b96b92bd6715caa0
Undiess <nguyen.andy67@gmail.com>
2020-05-03 01:39:15 +0930

    changed syntax

--- index.js
+++ index.js
@@ -3,53 +3,45 @@ const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 
-function promptQuestions(){
-
-
-const questions = [
-    
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message:' What is the project Title: '
-    },
-    {
-        type: 'input',
-        name: 'projectDescription',
-        message:' What is the project Desscription : '
-    },
-    {
-        type: 'input',
-        name: 'Installation ',
-        message:' Installation : '
-    },
-    {
-        type: 'input',
-        name: 'Usage',
-        message:' What is the Usage: '
-    },
-    {
-        type: 'input',
-        name: 'Contributing',
-        message:' Contribution: '
-    },
-    {
-        type: 'input',
-        name: 'GitHubreponame',
-        message:' What is the Git hub repo name: '
-    },
-    {
-        type: 'input',
-        name: 'githubemail',
-        message:' What is the github email: '
-    },
-
-]
-    
-
-return inquirer.prompt(questions)
-
-}
-
-promptQuestions()
-
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message:' What is the project Title: '
+        },
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message:' What is the project Desscription : '
+        },
+        {
+            type: 'input',
+            name: 'Installation ',
+            message:' Installation : '
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message:' What is the Usage: '
+        },
+        {
+            type: 'input',
+            name: 'Contributing',
+            message:' Contribution: '
+        },
+        {
+            type: 'input',
+            name: 'GitHubreponame',
+            message:' What is the Git hub repo name: '
+        },
+        {
+            type: 'input',
+            name: 'githubemail',
+            message:' What is the github email: '
+        },
+    ])
+    .then(answers => {
+        console.log(answers)
+    })
+   
