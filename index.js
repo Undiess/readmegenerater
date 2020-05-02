126abe90823d74c890e444d985b14445f4835cbe
Undiess <nguyen.andy67@gmail.com>
2020-05-03 02:52:00 +0930

    updated readme

--- index.js
+++ index.js
@@ -3,45 +3,94 @@ const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 
-inquirer 
-    .prompt([
-        {
-            type: 'input',
-            name: 'projectTitle',
-            message:' What is the project Title: '
-        },
-        {
-            type: 'input',
-            name: 'projectDescription',
-            message:' What is the project Desscription : '
-        },
-        {
-            type: 'input',
-            name: 'Installation ',
-            message:' Installation : '
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message:' What is the Usage: '
-        },
-        {
-            type: 'input',
-            name: 'Contributing',
-            message:' Contribution: '
-        },
-        {
-            type: 'input',
-            name: 'GitHubreponame',
-            message:' What is the Git hub repo name: '
-        },
-        {
-            type: 'input',
-            name: 'githubemail',
-            message:' What is the github email: '
-        },
-    ])
-    .then(answers => {
-        console.log(answers)
-    })
-   
+
+    function promptUser() {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'projectTitle',
+                message:' What is the project Title: '
+            },
+            {
+                type: 'input',
+                name: 'projectDescription',
+                message:' What is the project Desscription : '
+            },
+            {
+                type: 'input',
+                name: 'Installation ',
+                message:' Installation : '
+            },
+            {
+                type: 'input',
+                name: 'Usage',
+                message:' What is the Usage: '
+            },
+            {
+                type: 'input',
+                name: 'Contributing',
+                message:' Contribution: '
+            },
+            
+            {
+                type: 'input',
+                name: 'tests',
+                message:' tests: '
+            },
+            {
+                type: 'input',
+                name: 'GitHubreponame',
+                message:' What is the Git hub repo name: '
+            },
+            {
+                type: 'input',
+                name: 'githubemail',
+                message:' What is the github email: '
+            },
+        ]);
+      }
+
+      function generateHTML(answers) {
+        return `
+      Contents of this file 
+      ----------------------
+      -Project 
+      -Description
+      -Installation 
+      -usage 
+      -contribution
+      -tests
+      -github
+      
+      Project 
+      ------
+      ${answers.projectTitle}
+      
+      Discription 
+      -----------
+      ${answers.projectDescription}
+
+      installation 
+      -------------
+      ${answers.Installation }
+      
+      usage 
+      ----
+      ${answers.Usage}
+
+      Contribution
+      ------------
+      ${answers.Contributing}
+
+      tests
+      -----
+      ${answers.tests}
+
+      github 
+      ------
+      repo name: ${answers.GitHubreponame}
+      email: ${answers.githubemail}
+      `
+      ;
+      }
+
