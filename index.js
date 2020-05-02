58f7ff15a45f663904426e81bdab8a7acd3ee0f9
Undiess <nguyen.andy67@gmail.com>
2020-05-03 01:22:30 +0930

    questions

--- index.js
+++ index.js
@@ -6,16 +6,50 @@ const axios = require("axios");
 function promptQuestions(){
 
 
-const questions = [{
-    type: 'input',
-    name: 'projectTitle',
-    message:' What is the project Title: '
-    }]
+const questions = [
+    
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message:' What is the project Title: '
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message:' What is the project Desscription : '
+    },
+    {
+        type: 'input',
+        name: 'Installation ',
+        message:' Installation : '
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message:' What is the Usage: '
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message:' Contribution: '
+    },
+    {
+        type: 'input',
+        name: 'GitHubreponame',
+        message:' What is the Git hub repo name: '
+    },
+    {
+        type: 'input',
+        name: 'githubemail',
+        message:' What is the github email: '
+    },
+
+]
     
 
 return inquirer.prompt(questions)
+
 }
 
 promptQuestions()
-
 
